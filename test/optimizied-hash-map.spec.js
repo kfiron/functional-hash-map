1a89026eb586d17013bbae4dca44e222431850dc
kfirb <kfirb@wix.com>
2016-12-14 12:11:51 +0200

    reafctor duplicates

--- test/optimizied-hash-map.spec.js
+++ test/optimizied-hash-map.spec.js
@@ -7,22 +7,26 @@ describe('optimized hash map', () => {
     this.map = factory.newMap();
   });
 
+  const key = 'key';
+  const stringValue = 'value';
+
   it('should return null for non exist entry', function() {
-    expect(this.map.get('key')).to.be.undefined;
+    expect(this.map.get(key)).to.be.undefined;
   });
 
-  it('should insert and get the key', function() {
-    this.map.set('key', 'value');
-    expect(this.map.get('key')).to.be.equal('value');
+  it('should insert and get the key which is a string', function() {
+    this.map.set(key, 'value');
+    expect(this.map.get(key)).to.be.equal(stringValue);
   });
+
 
   it('should return false for check existence using has ', function() {
-    expect(this.map.has('key')).to.be.false;
+    expect(this.map.has(key)).to.be.false;
   });
 
   it('should return true for check existence using has ', function() {
     this.map.set('key', 'value');
-    expect(this.map.has('key')).to.be.true;
+    expect(this.map.has(key)).to.be.true;
   });
 
 
