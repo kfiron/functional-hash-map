11b491358729495fea2253b71cdcbe61bc75a427
kfirb <kfirb@wix.com>
2016-12-14 12:28:01 +0200

    embedded mongo 3.2.1

--- test/optimizied-hash-map.spec.js
+++ test/optimizied-hash-map.spec.js
@@ -7,26 +7,31 @@ describe('optimized hash map', () => {
     this.map = factory.newMap();
   });
 
-  const key = 'key';
-  const stringValue = 'value';
+  const stringKey = 'key';
+  const intKey = 0;
+  const value = 'value';
 
   it('should return null for non exist entry', function() {
-    expect(this.map.get(key)).to.be.undefined;
+    expect(this.map.get(stringKey)).to.be.undefined;
   });
 
   it('should insert and get the key which is a string', function() {
-    this.map.set(key, 'value');
-    expect(this.map.get(key)).to.be.equal(stringValue);
+    this.map.set(stringKey, value);
+    expect(this.map.get(stringKey)).to.be.equal(value);
   });
 
-
   it('should return false for check existence using has ', function() {
-    expect(this.map.has(key)).to.be.false;
+    expect(this.map.has(stringKey)).to.be.false;
   });
 
   it('should return true for check existence using has ', function() {
-    this.map.set('key', 'value');
-    expect(this.map.has(key)).to.be.true;
+    this.map.set(stringKey, value);
+    expect(this.map.has(stringKey)).to.be.true;
+  });
+  it('should map to different types by providing predicate', function(){
+    this.map.set(stringKey, value);
+    this.map.map(val => val + 'extend');
+    expect(this.map.get(stringKey)).to.be.equal(value + 'extend');
   });
 
 
