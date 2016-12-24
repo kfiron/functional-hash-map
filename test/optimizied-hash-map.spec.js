bcf6770ebba9cda39c7636312cfa10861afa5018
kfirb <kfirb@wix.com>
2016-12-24 12:25:22 +0200

    starting map and refactor

--- test/optimizied-hash-map.spec.js
+++ test/optimizied-hash-map.spec.js
@@ -10,25 +10,34 @@ describe('optimized hash map', () => {
   const key = 'key';
   const stringValue = 'value';
 
-  it('should return null for non exist entry', function() {
-    expect(this.map.get(key)).to.be.undefined;
+
+  describe('get and set by key', function () {
+    it('should return null for non exist entry', function () {
+      expect(this.map.get(key)).to.be.undefined;
+    });
+    it('should insert and get the key which is a string', function () {
+      this.map.set(key, 'value');
+      expect(this.map.get(key)).to.be.equal(stringValue);
+    });
   });
 
-  it('should insert and get the key which is a string', function() {
-    this.map.set(key, 'value');
-    expect(this.map.get(key)).to.be.equal(stringValue);
+  describe('has()', function () {
+    it('should return false for check existence using has ', function () {
+      expect(this.map.has(key)).to.be.false;
+    });
+
+    it('should return true for check existence using has ', function () {
+      this.map.set('key', 'value');
+      expect(this.map.has(key)).to.be.true;
+    });
   });
 
-
-  it('should return false for check existence using has ', function() {
-    expect(this.map.has(key)).to.be.false;
+  describe('map()', function(){
+    it('map on empty map should return empty object', function(){
+      const newMap = this.map.map((k, v) => 'v');
+      expect(newMap.size()).to.be.equal(0);
+    });
   });
-
-  it('should return true for check existence using has ', function() {
-    this.map.set('key', 'value');
-    expect(this.map.has(key)).to.be.true;
-  });
-
 
 
 });
